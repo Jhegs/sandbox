a3c5b0a5735d976ab39d9afc9fa91df2c2dc052a
Cinthia Argote <cinthia.argote1998@gmail.com>
2025-04-05 00:49:36 -0400

    loading data from api url

--- cat-planet/src/pages/CatsPage.tsx
+++ cat-planet/src/pages/CatsPage.tsx
@@ -1,34 +1,61 @@
-import { ImageList, ImageListItem } from "@mui/material";
+import { Card, CardContent, Container, Grid, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 
-const catsList = [
-  {
-    id: 1,
-    name: "Arturito",
-    imgURL:
-      "https://cdn.shopify.com/s/files/1/0997/4496/articles/Capture_d37ce215-f0b0-4edb-b366-edbf14af3aed.PNG?v=1588962370",
-  },
-  {
-    id: 2,
-    name: "chiki",
-    imgURL:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsK6qpQ8uda58F9Ru9Ys3zDrQkvVG-bpBSPw&s",
-  },
-];
+const API_URL = "http://api.thecatapi.com/v1/breeds";
+// const API_URL = "https://api.thecatapi.com/v1/images/search";
+
+// const catsList_OLD = [
+//   {
+//     id: 1,
+//     name: "Arturito",
+//     imgURL:
+//       "https://cdn.shopify.com/s/files/1/0997/4496/articles/Capture_d37ce215-f0b0-4edb-b366-edbf14af3aed.PNG?v=1588962370",
+//   },
+//   {
+//     id: 2,
+//     name: "chiki",
+//     imgURL:
+//       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsK6qpQ8uda58F9Ru9Ys3zDrQkvVG-bpBSPw&s",
+//   },
+// ];
 
 const CatsPage = () => {
+  const [catsList, setCatList] = useState([]);
+
+  // Fetching data from the API
+  useEffect(() => {
+    const getCats = async () => {
+      try {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+        setCatList(data);
+        console.log("Cats data:", data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    getCats();
+  }, []);
+
   return (
-    <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
-      {catsList.map((catData) => (
-        <ImageListItem key={catData.id}>
-          <img
-            // srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-            src={catData.imgURL}
-            alt={catData.name}
-            loading="lazy"
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
+    <Container sx={{ padding: "20px", width: "100%" }}>
+      <Grid container spacing={2}>
+        {catsList.map((catData) => (
+          <Grid size={2}>
+            <Card>
+              <CardContent sx={{ width: "150px", height: "100%" }}>
+                <Typography variant="h5" component="div">
+                  {catData.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {catData.origin}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 };
 
